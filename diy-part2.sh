494acccbc6156928704a7e3c06ed7bc61d489210
lunjj <lunss-09@outlook.com>
2026-03-27 22:39:55 +0800

    编译固件附带仓库提交 sha

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,9 +22,12 @@
 # 临时解决Rust问题
 sed -i 's/ci-llvm=true/ci-llvm=false/g' feeds/packages/lang/rust/Makefile
 
-# add date in output file name
-sed -i -e '/^IMG_PREFIX:=/i BUILD_DATE := $(shell date +%Y%m%d)' \
-       -e '/^IMG_PREFIX:=/ s/\($(SUBTARGET)\)/\1-$(BUILD_DATE)/' include/image.mk
+# add date and git sha in output file name
+sed -i \
+  -e '/^IMG_PREFIX:=/i BUILD_DATE := $(shell date +%Y%m%d)' \
+  -e '/^IMG_PREFIX:=/i GIT_SHA := $(shell git rev-parse --short HEAD)' \
+  -e '/^IMG_PREFIX:=/ s/\($(SUBTARGET)\)/\1-$(BUILD_DATE)-$(GIT_SHA)/' \
+  include/image.mk
 
 # set ubi to 122M
 # sed -i 's/reg = <0x5c0000 0x7000000>;/reg = <0x5c0000 0x7a40000>;/' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1-ubootmod.dts
